e87a953ec0f7da6a733e86c2098a161117891722
Abhay <ak20024091@gmail.com>
2024-03-14 13:49:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+import Home from './components/Home/Home'
+import Main from './components/Main/Main'
+import Footer from './components/Footer/Footer'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <Navbar></Navbar>
+      <Home></Home>
+       <Main></Main>
+       <Footer></Footer>
+    </>
+  )
+}
+
+export default App
